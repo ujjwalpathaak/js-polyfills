ea3a82584c05bfc96c7c2d857e82d2bc84e535ed
ujjwal pathak <ujjwal.pathak@darwinbox.io>
2025-02-10 17:01:08 +0530

    added polyfills for call, bind, apply

--- polyfill.js
+++ polyfill.js
@@ -50,21 +50,75 @@
 
 // Polyfill for reduce()
 
-Array.prototype.myReduce = function (cb, value) {
-    var accum = value;
-    for (let i = 0; i < this.length; i++) {
-        accum = accum ? cb(this[i], accum) : this[i];
-    }
+// Array.prototype.myReduce = function (cb, value) {
+//     var accum = value;
+//     for (let i = 0; i < this.length; i++) {
+//         accum = accum ? cb(this[i], accum) : this[i];
+//     }
 
-    return accum;
-}
+//     return accum;
+// }
 
-let arr = [1, 2, 3, 4, 5];
-let cb = (curr, value) => {
-    return value + curr;
-}
-let ans = arr.myReduce(cb, 0);
+// let arr = [1, 2, 3, 4, 5];
+// let cb = (curr, value) => {
+//     return value + curr;
+// }
+// let ans = arr.myReduce(cb, 0);
 
-console.log(ans);
+// console.log(ans);
+
+// -------------------------------------------------------------------
+
+// let person1 = {
+//     firstName: "Ujjwal",
+//     secondName: "Pathak"
+// }
+
+// let person2 = {
+//     firstName: "Harshit",
+//     secondName: "Kapoor"
+// }
+
+// function getFullName (...args) {
+//     return this.firstName + " " + this.secondName + " " + JSON.stringify([...args]);
+// }
+
+// Function.prototype.customCall = function (context = {}, ...args){
+//     context.fn = this;
+//     return context.fn(...args);
+// }
+
+// custom call
+// console.log(getFullName.customCall(person1, 1, 2, 3))
+// console.log(getFullName.customCall(person2, 1, 2, 3))
+
+// call func
+// console.log(getFullName.call(person1))
+// console.log(getFullName.call(person2))
+
+
+// Polyfill for Apply()
+
+// Function.prototype.customApply = function (context = {}, args = []){
+//     context.fn = this;
+//     return context.fn(...args);
+// }
+
+// custom call
+// console.log(getFullName.customApply(person1, [3, 4, 5]))
+// console.log(getFullName.customApply(person2, [3, 4, 5]))
+
+// Polyfill for Bind()
+
+// Function.prototype.customBind = function (context = {}, ...args1){
+//     context.fn = this;
+//     return function (...args2) {
+//         return context.fn(...args1, ...args2);
+//     }
+// }
+
+// custom bind
+// const bindFunctionPerson1 = getFullName.customBind(person1, 3, 4, 5)
+// console.log(bindFunctionPerson1(1, 2, 3));
 
 // -------------------------------------------------------------------
