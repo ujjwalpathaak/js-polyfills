7679c36c53e046d817bb7ad14965812a15373d78
ujjwal pathak <ujjwal.pathak@darwinbox.io>
2025-02-11 14:16:35 +0530

    promise polyfill

--- polyfill.js
+++ polyfill.js
@@ -50,22 +50,22 @@
 
 // Polyfill for reduce()
 
-// Array.prototype.myReduce = function (cb, value) {
-//     var accum = value;
-//     for (let i = 0; i < this.length; i++) {
-//         accum = accum ? cb(this[i], accum) : this[i];
-//     }
+Array.prototype.myReduce = function (cb, value) {
+    var accum = value || 0;
+    for (let i = 0; i < this.length; i++) {
+        accum = cb(this[i], accum);
+    }
 
-//     return accum;
-// }
+    return accum;
+}
 
-// let arr = [1, 2, 3, 4, 5];
-// let cb = (curr, value) => {
-//     return value + curr;
-// }
-// let ans = arr.myReduce(cb, 0);
+let arr = [1, 2, 0, 1, 3];
+let cb = (curr, value) => {
+    return value + curr;
+}
+let ans = arr.myReduce(cb, );
 
-// console.log(ans);
+console.log(ans);
 
 // -------------------------------------------------------------------
 
@@ -96,7 +96,6 @@
 // console.log(getFullName.call(person1))
 // console.log(getFullName.call(person2))
 
-
 // Polyfill for Apply()
 
 // Function.prototype.customApply = function (context = {}, args = []){
@@ -122,3 +121,139 @@
 // console.log(bindFunctionPerson1(1, 2, 3));
 
 // -------------------------------------------------------------------
+
+// Polyfill for once()
+
+// let obj = {
+//     name: "Ujjwal"
+// }
+// let obj2 = {
+//     name: "Pathak"
+// }
+
+// function once (func) {
+//     let ran = false;
+//     return function () {
+//         if(!ran){
+//             func.call(...arguments)
+//             ran = true;
+//             return;
+//         }
+
+//         console.log("Already ran once");
+//     }
+// }
+
+// const hello = once(function () {
+//     console.log(this.name);
+// });
+// const hello2 = once(function () {
+//     console.log(this.name);
+// });
+
+// hello(obj)
+// hello(obj2)
+// hello2(obj2)
+
+// -------------------------------------------------------------------
+
+// Polyfill for memoize
+
+// function memoize (func, context) {
+//     let cache = new Map();
+//     return function (...args) {
+//         let key = JSON.stringify(args)
+//         console.log(key)
+//         if(!cache.has(key)){
+//             let result = func(...args);
+//             cache.set(key, result);
+//         }
+//         return cache.get(key);
+//     }
+// }
+
+// const f = memoize(function (a, b){
+//     console.log(a, b)
+//     for (let i=1; i <= 100000000; i++){}
+//     return a * b;
+// })
+
+// console.time("First call");
+// console.log(f(9467, 7649));
+// console.timeEnd("First call");
+
+// console.time("Second call");
+// console.log(f(9467, 7649));
+// console.timeEnd("Second call");
+
+// -------------------------------------------------------------------
+
+// Polyfill for promise
+
+// function MyPromise(executor) {
+//     let onResolve,
+//       onReject,
+//       isFullfilled = false,
+//       isRejected = false,
+//       isCalled = false,
+//       value;
+  
+//     function resolve(val) {
+//       isFullfilled = true;
+//       value = val;
+  
+//       if (typeof onResolve === "function") {
+//         onResolve(value);
+//         isCalled = true;
+//       }
+//     }
+  
+//     function reject(val) {
+//       isRejected = true;
+//       value = val;
+  
+//       if (typeof onReject === "function") {
+//         onReject(value);
+//         isCalled = true;
+//       }
+//     }
+  
+//     this.then = function (cb) {
+//       onResolve = cb;
+//       if (isFullfilled && !isCalled) {
+//         isCalled = true;
+//         onResolve(value);
+//       }
+//       return this;
+//     };
+  
+//     this.catch = function (cb) {
+//       onReject = cb;
+//       if (isRejected && !isCalled) {
+//         isCalled = true;
+//         onReject(value);
+//       }
+//       return this;
+//     };
+  
+//     try {
+//       executor(resolve, reject);
+//     } catch (err) {
+//       reject(err);
+//     }
+//   }
+  
+//   const examplePromise = new MyPromise((resolve, reject) => {
+//     setTimeout(() => {
+//         // reject(2);
+//         resolve(2);
+//     }, 1000)
+//   });
+  
+//   examplePromise
+//     .then((res) => {
+//       console.log("Resolved:", res);
+//     })
+//     .catch((err) => {
+//       console.error("Rejected:", err);
+//     });
